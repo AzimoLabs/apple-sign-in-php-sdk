59a8ae5cfd722620bd0941481622eb191e4a95b8
royee <guanhui07@gmail.com>
2023-10-22 20:40:17 +0800

    Update CryptographicAlgorithmEnum.php

--- src/Api/Enum/CryptographicAlgorithmEnum.php
+++ src/Api/Enum/CryptographicAlgorithmEnum.php
@@ -2,6 +2,12 @@
 
 namespace Azimo\Apple\Api\Enum;
 
+/**
+ * appleid 支持的 auth keys
+ * https://appleid.apple.com/auth/keys
+ * 之前是2种  86D88Kf eXaunmL
+ * 现在是 YuyXoY  fh6Bs8C W6WcOKB
+ */
 class CryptographicAlgorithmEnum
 {
     public const KID_86D88KF = '86D88Kf';
@@ -9,6 +15,9 @@ class CryptographicAlgorithmEnum
     public const KID_EXAUNML = 'eXaunmL';
 
     public const KID_YUYXOY = 'YuyXoY';
+    // add this two line code
+    public const KID_TYPE = 'fh6Bs8C';
+    public const KID_TYPE2 = 'W6WcOKB';
 
     public static function supportedAlgorithms(): array
     {
@@ -16,6 +25,9 @@ class CryptographicAlgorithmEnum
             self::KID_86D88KF,
             self::KID_EXAUNML,
             self::KID_YUYXOY,
+            // add this two line code
+            self::KID_TYPE,
+            self::KID_TYPE2,
         ];
     }
 
